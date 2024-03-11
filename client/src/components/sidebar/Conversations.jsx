eda969e62b522d83ee56b0d3ad2299f669dcd274
samellow <sammykaris48@gmail.com>
2024-03-11 11:49:35 +0300

    Worked on messages and conversation hooks

--- client/src/components/sidebar/Conversations.jsx
+++ client/src/components/sidebar/Conversations.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import Conversation from './Conversation'
+import useGetConversations from '../../hooks/useGetConversations'
+import { getRandomEmoji } from '../../utils/emoji'
 const Conversations = () => {
-  return (
-    <div className='py-2 flex flex-col overflow-auto'>
- 			<Conversation />
- 			<Conversation />
-			<Conversation /> 		
-        	<Conversation />
-			<Conversation /> 			
-            <Conversation />
-		</div>
+	const {conversations, loading} = useGetConversations()
+    console.log(conversations)
+	return (
+		<div className='py-2 flex flex-col overflow-auto'>
+		{conversations.map((conversation, idx) => (
+			<Conversation
+				key={conversation._id}
+				conversation={conversation}
+				emoji={getRandomEmoji()}
+				lastIdx={idx === conversations.length - 1}
+			/>
+		))}
+
+		{loading ? <span className='loading loading-spinner mx-auto'></span> : null}
+	</div>
   )
 }
 
